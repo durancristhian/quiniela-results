5bb9f9e14f8a89c63db328cd5604f6cb8b38a9e0
Cristhian Duran <durancristhian@gmail.com>
2016-06-27 01:58:55 -0300

    test(Error cases): Add nock as HTTP mocking library to complete some missing tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,5 @@
 const getResults = require('../lib')
+const nock = require('nock')
 const test = require('tape')
 
 test('check results', (assert) => {
@@ -7,6 +8,30 @@ test('check results', (assert) => {
   getResults(today)
     .then(results => {
       assert.equal(Array.isArray(results), true, 'results should be an array')
+      assert.end()
+    })
+})
+
+test('check invalid date parameter', (assert) => {
+  let today = 'this is not a valid date'
+
+  getResults(today)
+    .catch(error => {
+      assert.equal(typeof error, 'object', 'error should be an object')
+      assert.end()
+    })
+})
+
+test('check errors', (assert) => {
+  let today = (new Date()).toISOString().substring(0, 10)
+
+  nock('http://www.dejugadas.com/quinielas/datospizarra.php')
+    .get('*')
+    .reply(404)
+
+  getResults(today)
+    .catch(error => {
+      assert.equal(typeof error, 'object', 'error should be an object')
       assert.end()
     })
 })
