d0b8c37649de732150d974a3c25aa7afb8168ddc
Cristhian Duran <durancristhian@gmail.com>
2016-06-30 02:29:19 -0300

    test(library results): Check for every result's item type

--- test/index.test.js
+++ test/index.test.js
@@ -8,7 +8,7 @@ test('check results', (assert) => {
   getResults(today)
     .then(results => {
       assert.equal(Array.isArray(results), true, 'results should be an array')
-      results.forEach(i => assert.equal(typeof i, 'object', 'every item in results should be an object'))
+      results.every(i => assert.ok(typeof i === 'object', 'every item in results should be an object'))
       assert.end()
     })
 })
